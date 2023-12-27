0558be057f24472cb496423f92ed1c4fbdb22ead
Jorge Mejia <jorgemejia2304@gmail.com>
2023-12-27 10:03:26 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Humanizer;
+Console.WriteLine("Por favor, ingrese su nombre: ");
+var nombre = Console.ReadLine();
+Console.WriteLine("Por favor, ingrese su apellido: ");
+var apellido = Console.ReadLine();
+Console.WriteLine("Por favor, ingrese su edad: ");
+var edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Hola {nombre} {apellido}, tu edad es {edad.ToWords(new System.Globalization.CultureInfo("es"))} años");
